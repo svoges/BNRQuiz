961f535adde878b244a32cf94100e16a40269601
Steffan Voges <steffan@berkeley.edu>
2014-06-30 20:52:32 -0700

    Initial Commit

---
+++ Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Steffan Voges on 6/30/14.
+//  Copyright (c) 2014 Steffan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Steffan Voges on 6/30/14.
+//  Copyright (c) 2014 Steffan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
