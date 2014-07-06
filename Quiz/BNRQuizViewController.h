8b1e72770dfc228dfe582a741dd904fe40b601ab
Steffan Voges <steffan@berkeley.edu>
2014-07-06 12:59:25 -0700

    finished quiz app

---
+++ Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Steffan Voges on 6/30/14.
+//  Copyright (c) 2014 Steffan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
---
+++ Quiz/BNRQuizViewController.m
@@ -0,0 +1,77 @@
+//
+//  BNRQuizViewController.m
+//  Quiz
+//
+//  Created by Steffan Voges on 6/30/14.
+//  Copyright (c) 2014 Steffan. All rights reserved.
+//
+
+#import "BNRQuizViewController.h"
+
+@interface BNRQuizViewController ()
+
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+@end
+
+@implementation BNRQuizViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    //Call the init method implemented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        // Create two arrays filled with questions and answers and make pointers to them
+        self.questions = @[@"What is cognac made from?",
+                           @"What is 7+7?",
+                           @"What's the best job in the world?"];
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Gametime"];
+        
+    }
+    
+    
+    // Return address of the new object
+    return self;
+    
+}
+
+- (IBAction)showQuestion:(id)sender 
+{
+    // Step to the next question
+    self.currentQuestionIndex++;
+    
+    // Am I the last question?
+    if (self.currentQuestionIndex == [self.questions count]) {
+        // Go back the first question
+        self.currentQuestionIndex = 0;
+    }
+    
+    // Get the string at that index in the questions array
+    NSString *question = self.questions[self.currentQuestionIndex];
+    
+    // Display the string in the question label
+    self.questionLabel.text = question;
+    
+    // Reset the answer label
+    self.answerLabel.text = @"???";
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    // What is the answer to the current question?
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    
+    //Display it in the answer label
+    self.answerLabel.text = answer;
+    
+}
+@end
